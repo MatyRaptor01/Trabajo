5746dea277f2b9c648917a08ad860eead3818b50
MatyRaptor01 <matiaspineda01@gmail.com>
2026-07-01 17:43:29 -0400

    Delete tienda_nexo-main directory

--- tienda_nexo-main/appnexo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Producto, Pedido, ListaDeseos, DireccionEnvio, MetodoPago
-
-admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(Pedido)
-admin.site.register(ListaDeseos)
-admin.site.register(DireccionEnvio)
-admin.site.register(MetodoPago)
